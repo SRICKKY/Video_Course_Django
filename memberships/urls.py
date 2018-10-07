ec266d1ad25df74dbd88e44e4706f70ad8e257ee
SRICKKY <kumar_srikant@outlook.com>
2018-10-07 23:09:16 +0530

    Fixed bunch of files and worked on profile view

--- memberships/urls.py
+++ memberships/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import MembershipSelectView, PaymentView
+from .views import (
+	MembershipSelectView, 
+	PaymentView, 
+	updateTransactionRecords,
+	profile_view,
+	cancelSubscription
+	)
 
-app_name = 'memeberships'
+app_name = 'memberships'
 
 urlpatterns = [
-	path('', MembershipSelectView.as_view(),name='select'),
-	path('payment', PaymentView,name='payment')
+    path('', MembershipSelectView.as_view(), name='select'),
+    path('payment/', PaymentView, name='payment'),
+    path('update-transactions/<subscription_id>/', updateTransactionRecords, name='update-transactions'),
+    path('profile/', profile_view, name='profile'),
+    path('cancel/', cancelSubscription, name='cancel')
 ]
